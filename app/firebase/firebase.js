950c3c9cbe6d0f9356959b0764670a0a3deaad8e
patemit <patelmit8292@gmail.com>
2024-08-13 16:29:45 -0400

    Implemented Firebase files and .env files

---
+++ app/firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics"; // Import if you're using analytics
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Add this line
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const storage = getStorage(app);
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null; // Initialize analytics if it's running in the browser
+
+export { storage, analytics };
