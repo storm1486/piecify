4e0e0eb9974295fb48e9193a0b1c4c03583a74de
storm1486 <patelmit8292@gmail.com>
2025-04-29 00:01:20 -0400

    fixed error for build

--- app/page.jsx
+++ app/page.jsx
@@ -1036,7 +1036,7 @@ export default function Home() {
                 </div>
                 <div className="relative flex justify-center text-sm">
                   <span className="px-2 bg-white text-gray-500">
-                    Don't have an account?
+                    Don&apos;t have an account?
                   </span>
                 </div>
               </div>
